98e928be41ba014df77bd7651e41b192a6b5060a
Stefanie <stefanien001@gmail.com>
2021-06-22 17:06:55 -0500

    working on displaying results and collecting correct information

--- script.js
+++ script.js
@@ -16,15 +16,44 @@ const findPokemon = async (input) => {
     console.log(response.data)
 
     // create variables for the info
-    const name = response.data.name
-    const id = response.data.id
-    const imageURL = response.data.sprites.other.official-artwork.front_default
-    const type1 = response.data.types.0.type.name
-    const type2 = response.data.types.1.type.name
-    const height = response.data.height
-    const weight = response.data.weight
+    
+    //append html elements to the page
+
+    const pokemonInfo = document.createElement("div")
+    
+    const name = document.createElement("h2")
+    const id = document.createElement("p")
+    const types = document.createElement("p")
+    const height = document.createElement("p")
+    const weight = document.createElement("p")
+    const image = document.createElement("img")
+    
+    // assign data from api
+    name.innerText = response.data.name
+    id.innerText = `Id# ${response.data.id}`
+    // const imageURL = response.data.sprites.other.official-artwork.front_default
+    // image.setAttribute("src", imageURL)
+    // image.setAttribute("alt", `${name}`)
+    const type1 = response.data.types[0].type.name
+    const type2 = response.data.types[1].type.name
+    //Some logic here for if only one type?
+    if (type2 === undefined) {
+      types.innerText = `Type: ${type1}`
+    } else {
+      types.innerText = `Type: ${type1}, ${type2}`
+    }
+    height.innerText = `Height: ${response.data.height}dm`
+    weight.innerText = `Weight: ${response.data.weight}`
 
     // create html elements and show results on page
+    // pokemonDataContainer.append(image)
+    pokemonDataContainer.append(pokemonInfo)
+    pokemonInfo.append(name)
+    pokemonInfo.append(id)
+    pokemonInfo.append(types)
+    pokemonInfo.append(height)
+    pokemonInfo.append(weight)
+
 
   } catch (error) {
     console.error(error)
